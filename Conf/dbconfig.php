390ef23996ccb167244db530656b140a63c0e1ff
aizsfgk <Fboy1992@users.noreply.github.com>
2016-10-09 11:34:35 +0800

    Update dbconfig.php

--- Conf/dbconfig.php
+++ Conf/dbconfig.php
@@ -7,7 +7,7 @@ return array(
     "DB_HOST"                       => "127.0.0.1", //数据库连接主机  如127.0.0.1
     "DB_PORT"                       => 3306,        //数据库连接端口
     "DB_USER"                       => "root",      //数据库用户名
-    "DB_PASSWORD"                   => "aishifeng123",          //数据库密码
+    "DB_PASSWORD"                   => "password",          //数据库密码
     "DB_DATABASE"                   => "wish",          //数据库名称
     "DB_PREFIX"                     => "sf_",          //表前缀
     //"DB_FIELD_CACHE"                => 1,           //字段缓存
